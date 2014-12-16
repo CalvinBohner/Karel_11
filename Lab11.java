b2ecef0aaaa86b7810fce47c575ef526a533191f
Calvin Bohner <calvin_bohner@asl.org>
2014-12-16 13:51:11 +0000

    iadfsa
    asdfasdf

--- Lab11.java
+++ Lab11.java
@@ -6,12 +6,38 @@
  *
  */
 
+import edu.fcps.Digit;
 import edu.fcps.karel2.Display; 
 
 public class Lab11 {
 	 
 	 public static void main(String[] args) {
-	
+		
+		 
+		 
+         Display.setSize(42, 37);
+         Display.setSpeed(10);
+		 
+		 
+		 
+		 Digit day1 = new Two(1,35);
+		 Digit day2 = new Zero(8, 35);
+		 Digit month1 = new Zero(1, 25);
+		 Digit month2 = new Four(8, 25);
+		 Digit year1 = new One(1, 15);
+		 Digit year2 = new Nine( 8, 15);
+		 Digit year3 = new Nine( 15, 15);
+		 Digit year4 = new Nine( 22, 15);
+		 
+		 day1.display();
+		 day2.display();
+		 month1.display();
+		 month2.display();
+		 year1.display();
+		 year2.display();
+		 year3.display();
+		 year4.display();
+		 
 	 }
 	 
  }
